5679db1133cea0b63313b21475d22e2ad50a3f74
Jakobb32866 <jaszoch@gmail.com>
2026-03-23 19:03:49 +0100

    Added rental class

--- s32866_apbd_c3/Config/GlobalSettings.cs
+++ s32866_apbd_c3/Config/GlobalSettings.cs
@@ -2,11 +2,18 @@ namespace s32866_apbd_c3.Config;
 
 public static class GlobalSettings
 {
+    public static int CurrentDay { get; private set; } = 1;
+    
     public static int StudentMaxRentals { get; private set; } = 2;
     public static int EmployeeMaxRentals { get; private set; } = 5;
+    public static int RentalTime { get; private set; } = 7;
+    public static int LateFeePerDay { get; private set; } = 10; //PLN
 
 
-
+    public static void NextDay()
+    {
+        CurrentDay++;
+    }
 
     //TODO
     public static void UpdateMaxRentals()
---
+++ s32866_apbd_c3/Model/Rental/Rental.cs
@@ -0,0 +1,37 @@
+using s32866_apbd_c3.Config;
+
+namespace s32866_apbd_c3.Model.Rental;
+
+using s32866_apbd_c3.Model.User;
+using s32866_apbd_c3.Model.Equipment;
+
+
+public class Rental
+{
+    public int rentedDay { get; }
+    public int returnDay { get; }
+    public int returnedDay { get; private set; }
+    public User Renter { get; }
+    public Equipment Device { get; }
+    public int LateFee { get; private set; }
+
+    public Rental(User user, Equipment equipment)
+    {
+        rentedDay = GlobalSettings.CurrentDay;
+        returnDay = rentedDay + GlobalSettings.RentalTime;
+        Renter = user;
+        Device = equipment;
+    }
+
+    public void Return()
+    {
+        returnedDay = GlobalSettings.CurrentDay;
+        if (returnedDay - returnDay > 0)
+        {
+            LateFee = GlobalSettings.LateFeePerDay * (returnedDay - returnDay);
+            return;
+        }
+
+        LateFee = 0;
+    }
+}
